ac789adf10d6af654e06b87555ba760af7ef1906
Leang <leang@groundcontrol.(none)>
2015-05-19 18:02:21 -0600

    updated to use back as arm and start as disarm to match the crazyflie

--- src/control.cpp
+++ src/control.cpp
@@ -1,4 +1,4 @@
-/* This file has been created to try and fly the quadcopter using the joystic */
+/* This file has been created to try and fly the quadcopter using the joystick */
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
@@ -11,13 +11,13 @@
 int roll, pitch, yaw, minRange = 1100,
 midRange = 1500,
 maxRange = 1900,
-thrust = minRange,
-toggle[2] = {1500,1099};
-
-int joy_a_,joy_b_, joy_lb_;
+thrust = minRange;
+//toggle[2] = {1500,1099};
+stableMode = 1500;
+int joy_a_,joy_b_, joy_lb_, joy_back_, joy_start_;
 
 bool armed = false;
-
+bool killSwitch = false;
 roscopter::RC rc_out;
 
 void twist_callback(const geometry_msgs::Twist& twist_msg_in) //this also scales the joystick reading
@@ -30,9 +30,12 @@ void twist_callback(const geometry_msgs::Twist& twist_msg_in) //this also scales
 
 void joy_callback(const sensor_msgs::Joy& joy_msg_in) //this also scales the joystick reading
 {
-        joy_a_ = joy_msg_in.buttons[0];         //arms the robot
-        joy_b_ = joy_msg_in.buttons[1];         //disarms the robot
-	joy_lb_ = toggle[ joy_msg_in.buttons[4]];     //toggle flight mode
+        //joy_a_ = joy_msg_in.buttons[0];         //arms the robot
+        //joy_b_ = joy_msg_in.buttons[1];         //disarms the robot
+	    //joy_lb_ = toggle[ joy_msg_in.buttons[4]];     //toggle flight mode
+	    joy_back_ = joy_msg_in.buttons[6];             //arm
+        joy_start_ = joy_msg_in.buttons[7];            //disarm
+
 
 }
 
@@ -63,24 +66,25 @@ int main(int argc, char** argv)
 
 	//ros::ServiceClient client = node.serviceClient<roscopter::APMCommand>("apm/command");
 	//roscopter::APMCommand srv;
-        ros::ServiceClient armClient = node.serviceClient<std_srvs::Empty>("/arm");
+    ros::ServiceClient armClient = node.serviceClient<std_srvs::Empty>("/arm");
 	ros::ServiceClient disarmClient = node.serviceClient<std_srvs::Empty>("/disarm");
-        std_srvs::Empty srv;
+	std_srvs::Empty srv;
 
 	float dead_zone = .15;
 	
 	while(ros::ok())
 	{
 	ros::spinOnce();
-		if(joy_a_ == true && !armed)
+		if(joy_back_ == true && !armed)
 		{
 			//resetController(rc_pub);
 			//srv.request.command = CMD_ARM;
 			//if(client.call(srv)){;}
 			if(armClient.call(srv)){;}
 			armed = !armed;
+			thrust = 0;
 		}
-		else if(joy_b_ == true && armed)
+		else if(joy_start_ == true && armed)
 		{
 			//resetController(rc_pub);
 			//srv.request.command = CMD_DISARM;
@@ -94,7 +98,7 @@ int main(int argc, char** argv)
 			rc_out.channel[1] = pitch;
 			rc_out.channel[2] = thrust;
 			rc_out.channel[3] =  yaw;
-			rc_out.channel[4] = joy_lb_;
+			rc_out.channel[4] = stableMode;
 	}
 
 	rc_pub.publish(rc_out);
