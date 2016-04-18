a79e25032e02ede245f78b24d5a1f5f03867c588
damanfb <daman.bareiss@utah.edu>
2016-04-18 22:47:40 +0000

    added trim offsets

--- src/darc_custom_quad_node.cpp
+++ src/darc_custom_quad_node.cpp
@@ -1,17 +1,21 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
+#include <sensor_msgs/Imu.h>
+
 #include <std_srvs/Empty.h>
 #include <iostream>
 #include <Eigen/Geometry>
 #include <std_msgs/Float32.h>
 
 #include <mavros_msgs/AttitudeTarget.h>
+#include <mavros_msgs/State.h>
+
 #include <math.h>
 #include <vector>
 #include <numeric>
 
-const float MAX_ANGLE = 20.0 * M_PI / 180.0;  // rad
+const float MAX_ANGLE = 15.0 * M_PI / 180.0;  // rad
 const float MAX_YAW_RATE = 60 * M_PI / 180.0;
 
 float rx, ry, vz, vw;
@@ -29,6 +33,20 @@ void twist_callback(const geometry_msgs::Twist& twist_msg_in) { //this also scal
   vz = ((vz < 0.0) ? 0.0 : ((vz > 1.0) ? 1.0 : vz));
 }
 
+float rz_init = 0.0;
+void imu_callback(const sensor_msgs::Imu& imu_in) {
+  float x = imu_in.orientation.x;
+  float y = imu_in.orientation.y;
+  float z = imu_in.orientation.z;
+  float w = imu_in.orientation.w;
+  rz_init = 2.0 * atan2(w, z);// - M_PI;
+}  // imu_callback
+
+bool offboard_mode = false;
+void mode_callback(const mavros_msgs::State& state) {
+  offboard_mode = !state.mode.compare("OFFBOARD") && state.armed;
+}  // mode_callback
+
 int main(int argc, char** argv) {
   ros::init(argc, argv, "darc_custom_quad_node");
   ros::NodeHandle node;
@@ -37,28 +55,67 @@ int main(int argc, char** argv) {
   ros::Subscriber twist_sub;
   twist_sub = node.subscribe("new_u", 1, twist_callback);
 
+  ros::Subscriber imu_sub;
+  imu_sub = node.subscribe("/mavros/imu/data", 1, imu_callback);
+
+  ros::Subscriber mode_sub;
+  mode_sub = node.subscribe("/mavros/state", 1, mode_callback);
+
   ros::Publisher set_pub = node.advertise<mavros_msgs::AttitudeTarget>("/mavros/setpoint_raw/attitude", 1);
   mavros_msgs::AttitudeTarget set_out;
   set_out.type_mask = 7;  // Ignore rates
 
-  float rz = 0.0;
-  rz = -M_PI / 2.0;
+  double roll_trim;
+  if (node.getParam("/roll_trim", roll_trim)) {;} else {
+    ROS_ERROR("Set Roll Trim"); return 0; }
+
+  double pitch_trim;
+  if (node.getParam("/pitch_trim", pitch_trim)) {;} else {
+    ROS_ERROR("Set Pitch Trim"); return 0; }
+
+  double yaw_trim;
+  if (node.getParam("/yaw_trim", yaw_trim)) {;} else {
+    ROS_ERROR("Set Yaw Trim"); return 0; }
+
+  ros::Duration(3.0).sleep();
+  ros::spinOnce();
+  ROS_ERROR("Ready");
+  float rz = rz_init;
+
   while (ros::ok()) {
     ros::spinOnce();
 
-    rz += vw / 100.0;
-    Eigen::Matrix3f m;
-    m = Eigen::AngleAxisf(rz, Eigen::Vector3f::UnitZ()) *
-        Eigen::AngleAxisf(ry, Eigen::Vector3f::UnitY()) *
-        Eigen::AngleAxisf(rx, Eigen::Vector3f::UnitX());
-    Eigen::Quaternionf q(m);
-    set_out.orientation.x = q.x();
-    set_out.orientation.y = q.y();
-    set_out.orientation.z = q.z();
-    set_out.orientation.w = q.w();
-
-    set_out.thrust = vz;
-
+    if (offboard_mode) {
+      rz += vw / 100.0;
+      if (rz < 0.0) {
+        rz = 2.0 * M_PI + rz;
+      }
+      if (rz > 2.0 * M_PI) {
+        rz = fmod(rz, 2.0 * M_PI);
+      }
+      Eigen::Matrix3f m;
+      m = Eigen::AngleAxisf(rz, Eigen::Vector3f::UnitZ()) *
+          Eigen::AngleAxisf(ry, Eigen::Vector3f::UnitY()) *
+          Eigen::AngleAxisf(rx, Eigen::Vector3f::UnitX());
+      Eigen::Quaternionf q(m);
+      set_out.orientation.x = q.x();
+      set_out.orientation.y = q.y();
+      set_out.orientation.z = q.z();
+      set_out.orientation.w = q.w();
+  
+      set_out.thrust = vz;
+      
+    } else {
+      rz = rz_init;
+      Eigen::Matrix3f m;
+      m = Eigen::AngleAxisf(rz, Eigen::Vector3f::UnitZ());
+      Eigen::Quaternionf q(m);
+      set_out.orientation.x = q.x();
+      set_out.orientation.y = q.y();
+      set_out.orientation.z = q.z();
+      set_out.orientation.w = q.w();
+      set_out.thrust = 0.0;
+    }
     set_out.header.stamp = ros::Time::now();
     set_pub.publish(set_out);
 
